96139c6e609f93aeb17ad63ae3a3399aff3a1c14
Your Name <you@example.com>
2023-11-15 07:03:09 -0800

    task o printf

---
+++ print.s.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <string.h>
+#include <stdarg.h>
+
+/**
+ * printf_string - prints string.
+ * @val: argument.
+ * Return: the length of the string.
+ */
+int print_string(va_list val)
+{
+	char *str;
+	int i;
+	int length;
+
+	str = va_arg(val, char*);
+	if (str == NULL)
+	{
+		str = "(null)";
+		length = _strlen(str);
+		for (i = 0; i < length; i++)
+			_putchar(str[i]);
+		return (length);
+	}
+	else
+	{
+		length = _strlen(str);
+		for (i = 0; i < length; i++)
+		       _putchar (str[i]);	
+		return (length);
+	}
+}
